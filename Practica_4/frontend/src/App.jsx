7178d434387622a8e29494e3b3360a0f7370ef12
Josue Castro <3944527630101@ingenieria.usac.edu.gt>
2026-03-22 17:43:25 -0600

    App y login

--- Practica_4/frontend/src/App.jsx
+++ Practica_4/frontend/src/App.jsx
@@ -1,2 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login"
+import Login from "./pages/Login"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Practica_4/frontend/src/pages/Login.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
+function Login() {
+    const [registro, setResgistro] = useState('')
+    const [contrasena , setContrasena ] = useState('')
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post("http://localhost:3000/api/auth/login", {
+                registro_academico: registro,
+                contrasena: contrasena
+            })
+localStorage.setItem("token", response.data.token)
+localStorage.setItem("user", JSON.stringify(response.data.user))
+
+alert("Login Correcto")
+        }
+        catch (error) {
+            alert("Error en el login")
+        }
+    }
+
+    return (
+        <div>
+            <h2>Login</h2> 
+           <input 
+              type="text"
+                placeholder="Registro Academico"
+                value={registro}
+                onChange={(e) => setResgistro(e.target.value)}
+            />
+            <input  
+                type="password"
+                placeholder="Contraseña"
+                value={contrasena}
+                onChange={(e) => setContrasena(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    )
+}
+
+export default Login;
+
+
